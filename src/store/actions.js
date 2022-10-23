095c6cb6a1c27dae5d25d73004475637e3273399
Grigoriy Berezin <104319869+GrigoriyBerezin@users.noreply.github.com>
2022-10-23 20:14:38 +0300

    first commit

---
+++ src/store/actions.js
@@ -0,0 +1,23 @@
+import * as actionTypes from "./actionTypes";
+
+export function taskCompleted(id) {
+    return {
+        type: actionTypes.taskUpdated,
+        payload: { id, completed: true },
+    };
+}
+export function titleChanged(id) {
+    return {
+        type: actionTypes.taskUpdated,
+        payload: { id, title: `New title for ${id}` },
+    };
+}
+/*===New actions===*/
+
+export function taskDelete(id) {
+    return {
+        type: actionTypes.taskDelete,
+        payload: { id },
+    };
+}
+
